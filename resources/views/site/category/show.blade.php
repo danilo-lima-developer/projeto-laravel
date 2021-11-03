1af378ea8c6c9b003b165a2c7618f4f51288d4a6
Danilo Lima <danilolimas.pe@gmail.com>
2021-11-02 22:01:09 -0300

    Criando Rotas, Controllers e Views

---
+++ resources/views/site/category/show.blade.php
@@ -0,0 +1,1 @@
+Pagina Categoria : {{$slug}}
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::namespace('Site')->group(function(){
+    Route::get('/', 'HomeController');
+
+    Route::get('produtos', 'CategoryController@index');
+    Route::get('produtos/{slug}','CategoryController@show');
+
+    Route::get('blog', 'BlogController');
+
+    Route::view('sobre', 'site.about.index'); 
+
+    Route::get('contato', 'ContactController@index');
+    Route::post('contato', 'ContactController@form');
 });
