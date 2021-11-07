87fe3e449486e85e82d3778934a77ff6178373c3
Danilo Lima <danilolimas.pe@gmail.com>
2021-11-06 21:08:05 -0300

    Integrando front-end

--- routes/web.php
+++ routes/web.php
@@ -14,15 +14,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::namespace('Site')->group(function(){
-    Route::get('/', 'HomeController');
+    Route::get('/', 'HomeController')->name('site.home');
 
-    Route::get('produtos', 'CategoryController@index');
-    Route::get('produtos/{slug}','CategoryController@show');
+    Route::get('produtos', 'CategoryController@index')->name('site.products');
+    Route::get('produtos/{slug}','CategoryController@show')->name('site.products.category');
 
-    Route::get('blog', 'BlogController');
+    Route::get('blog', 'BlogController')->name('site.blog');
 
-    Route::view('sobre', 'site.about.index'); 
+    Route::view('sobre', 'site.about.index')->name('site.about'); 
 
-    Route::get('contato', 'ContactController@index');
-    Route::post('contato', 'ContactController@form');
+    Route::get('contato', 'ContactController@index')->name('site.contact');
+    Route::post('contato', 'ContactController@form')->name('site.contact.form');
 });
