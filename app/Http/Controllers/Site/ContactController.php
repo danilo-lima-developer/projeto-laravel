43cd9e79ed4998746473673314e796b8e88851d2
Danilo Lima <danilolimas.pe@gmail.com>
2021-11-07 01:45:42 -0300

    Envio de email e insert no banco, QUEUE

--- app/Http/Controllers/Site/ContactController.php
+++ app/Http/Controllers/Site/ContactController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers\Site;
 
+use App\Contact;
 use App\Http\Controllers\Controller;
+use App\Notifications\NewContact;
 use Illuminate\Http\Request;
+use Notification; //use Illuminate\Notifications\Notification;
 
 class ContactController extends Controller
 {
@@ -19,6 +22,10 @@ class ContactController extends Controller
 
     public function form(Request $request)
     {
-        ddd($request->all());
+        $contact = Contact::create($request->all());
+        Notification::route('mail', config('mail.from.address'))
+        ->notify(new NewContact($contact));
+
+        ddd($contact); // Retirar o método all para visualizar todos os parâmetros
     }
 }
---
+++ app/Notifications/NewContact.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+use App\Contact;
+
+class NewContact extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    protected $contact;
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct(Contact $contact)
+    {
+        $this->contact = $contact;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                    ->line('The introduction to the notification.')
+                    ->action('Notification Action', url('/'))
+                    ->line($this->contact->email)
+                    ->line($this->contact->name)
+                    ->line($this->contact->message);
+
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
